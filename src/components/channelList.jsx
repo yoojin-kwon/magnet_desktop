9f496afc50f98969e739a7a62dba19d85d53ad87
Yoojin Kwon <dbwlskwon@gmail.com>
2022-06-01 16:39:27 +0900

    wip

--- src/components/channelList.jsx
+++ src/components/channelList.jsx
@@ -24,12 +24,18 @@ const ChannelList = ({ channel, joinChannel, goToChat }) => {
 
   return (
     <List>
-      <div>#{channel.channelName}</div>
-      <div>{channel.channelComment}</div>
-      <button onClick={() => joinChannel(channel.createdAt)}>
-        {join ? 'Join!' : 'Join?'}
-      </button>
-      <button onClick={() => goToChat(channel.createdAt)}>Chat</button>
+      <div>
+        <Name># {channel.channelName}</Name>
+        <Comment>{channel.channelComment}</Comment>
+      </div>
+      <div>
+        <Button type='join' onClick={() => joinChannel(channel.createdAt)}>
+          {join ? 'Join! 🥳' : 'Join? 🤔'}
+        </Button>
+        <Button type='chat' onClick={() => goToChat(channel.createdAt)}>
+          Chat 💬
+        </Button>
+      </div>
     </List>
   );
 };
@@ -37,4 +43,33 @@ export default ChannelList;
 
 const List = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 22em;
+  margin-bottom: 1.2em;
+`;
+
+const Name = styled.div`
+  font-weight: 600;
+  margin-bottom: 0.4em;
+`;
+
+const Comment = styled.div`
+  font-size: 0.8em;
+  width: 10em;
+`;
+
+const Button = styled.button`
+  width: 5.5em;
+  height: 3em;
+  border-radius: 10em;
+  margin-left: 0.5em;
+  cursor: pointer;
+  border: transparent;
+  background-color: ${(props) =>
+    props.type === 'join' ? 'rgb(224, 155, 155)' : 'rgb(150, 186, 224)'};
+
+  &:hover {
+    opacity: 0.7;
+  }
 `;
