a4a78a3702eab236eadee69282fe79fcd11ba7eb
Yoojin Kwon <dbwlskwon@gmail.com>
2022-06-01 15:43:24 +0900

    Feat: 유저의 channel 참여 여부 표시

--- src/components/channelList.jsx
+++ src/components/channelList.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { firebaseDatabase, firebaseAuth } from '../service/firebase';
 
 const ChannelList = ({ channel, joinChannel, goToChat }) => {
+  const [join, setJoin] = useState(false);
+  const user = firebaseAuth.currentUser.uid;
+  useEffect(() => {
+    firebaseDatabase
+      .ref(`channels/${channel.createdAt}/members`) //
+      .get()
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const userId = Object.values(snapshot.val());
+          setJoin(userId.includes(user));
+        } else {
+          console.log('No data available');
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <List>
       <div>#{channel.channelName}</div>
       <div>{channel.channelComment}</div>
-      <button onClick={() => joinChannel(channel.createdAt)}>join</button>
+      <button onClick={() => joinChannel(channel.createdAt)}>
+        {join ? 'Join!' : 'Join?'}
+      </button>
       <button onClick={() => goToChat(channel.createdAt)}>Chat</button>
     </List>
   );
